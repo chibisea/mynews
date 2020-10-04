ef491710bbf9c956e95df2e1b10c81ae2aa0f74e
kaichi <chibisea@gmail.com>
2020-10-04 05:28:23 +0000

    kadai14

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -1,21 +1,29 @@
 <?php
-
 namespace App\Http\Controllers\Admin;
-
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Profile;
 class ProfileController extends Controller
 {
-    //
-     public function add()
-    {
-        return view('admin.profile.create');
-    }
+public function add()
+{
+return view('admin.profile.create');
+}
 
-    public function create()
+    public function create(Request $request)
     {
-        return redirect('admin/profile/create');
+      $this->validate($request, Profile::$rules);
+      $profile = new Profile;
+      $form = $request->all();
+     // フォームから送信されてきた_tokenを削除する
+      unset($form['_token']);
+      // フォームから送信されてきたimageを削除する
+      unset($form['image']);
+      // データベースに保存する
+      $profile->fill($form);
+      $profile->save();
+      return redirect('admin/profile/create');
     }
 
     public function edit()
