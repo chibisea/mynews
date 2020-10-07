fb4f69ed88738314a782a9330b7d7be499a998a5
kaichi <chibisea@gmail.com>
2020-10-07 15:13:34 +0000

    kadai16

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -3,7 +3,7 @@
 
 
 {{-- profile.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
-@section('title', 'ニュースの新規作成')
+@section('title', 'プロフィールの新規作成')
 
 {{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
