7315a9c254cd384270dacf95f24bf660518de8dd
kaichi <chibisea@gmail.com>
2020-10-09 11:29:00 +0000

    kadai16

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -1,11 +1,44 @@
 <?php
 
-namespace App\Http\Controllers\Admin;
+namespace APP\Http\Controller\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class ProfileController extends Controller
 {
-    //
+    public function add()
+    {
+        return view('admin.profile.create');
+    }
+    
+    public function create(Request $request)
+    {
+        $this->validate($request, mynews/app/Profile::$rules);
+        $profile = new Profile;
+        
+        unset($form['_token']);
+        unset($form['image']);
+        $profile->fill($form);
+        $profile->save();
+        return redirect('admin/profile/create');
+    }
+    
+    public function edit()
+    {
+        $profile = Profile::find($request->id);
+        if(empty($profile)){
+            abort(404);
+        }
+        return view('admin.profile.edit', ['profile_form' =>$profile]);
+    }
+    
+    public function update()
+    {
+        $this->validate($request, Profile::$rules);
+        $profile = Profile::find($request->id);
+        $profile_form = $request->all();
+        unset($profile_form['_token']);
+        return redirect('admin/profile/edit');
+    }
 }
