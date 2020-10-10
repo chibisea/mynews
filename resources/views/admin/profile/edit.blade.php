58335a3d9787b94915e8670298cfeb0ed140e467
kaichi <chibisea@gmail.com>
2020-10-10 08:17:42 +0000

    kadai17

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -50,7 +50,7 @@
                         <h2>プロフィール編集履歴</h2>
                         <ul class="list-group">
                             @if ($profile_form->histories != NULL)
-                                @foreach ($profile_form->profilehistories as $profilehistory)
+                                @foreach ($profile_form->profile_histories as $profilehistory)
                                     <li class="list-group-item">{{ $profilehistory->edited_at }}</li>
                                 @endforeach
                             @endif
